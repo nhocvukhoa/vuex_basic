6a821e2b958c2415c41ea58582766d4a66d6c15c
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-03-26 16:02:59 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background: rgb(219, 219, 219);
+	margin: 20px auto;
+	width: 90%;
+	max-width: 800px;
+}
+</style>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,47 @@
+<template>
+	<TheNavbar />
+  <div class="todo-list">
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import TheNavbar from '../components/TheNavbar.vue'
+
+const todos = ref([
+  {
+    id: 1,
+    title: 'Công việc 1',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'Công việc 2',
+    completed: false
+  },
+  {
+    id: 3,
+    title: 'Công việc 3',
+    completed: false
+  }
+])
+</script>
+
+<style>
+.todo-list ul {
+  padding: 0 10px 10px 10px;
+  list-style-type: none;
+}
+
+.todo-list li {
+  padding: 10px;
+  cursor: pointer;
+  margin: 10px 0;
+  border-radius: 4px;
+  background: rgb(240, 240, 240);
+  color: black;
+}
+</style>
